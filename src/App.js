65bf0955684e78a30a51ba65d65cf33e5be05fcb
sukhendra <mpct5jan1998@gmail.com>
2023-03-16 22:24:09 +0530

    props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Header from "./Components/Header";
 const App = () => {
     return (
         <div>
-           <Header />
+           {/* <Header title = "Text Utils"  aboutText = "About Us"/> */}
+           <Header title = "TextUtils"  aboutText = "About Us" btnText = "Click me!"/>
+           {/* <Header title = "TextUtils"  aboutText = "About Us" /> */}
+           {/* <Header  aboutText = "About Us" /> */}
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
-const Header = () => {
+
+const Header = (props) => {
   return (
     <section className="bg-gray-200 px-12 h-20">
       <nav className="flex justify-between items-center h-20">
 
         <div className="flex justify-around items-center w-96">
-          <section className="text-2xl font-bold">TextUtils</section>
+          <section className="text-2xl font-bold">{props.title}</section>
           <ul className="flex">
             <li className="w-20 hover:text-red-400 hover:underline cursor-pointer">Home</li>
-            <li className="w-20 hover:text-red-400 hover:underline cursor-pointer">About</li>
+            <li className="w-20 hover:text-red-400 hover:underline cursor-pointer">{props.aboutText}</li>
           </ul>
         </div>
 
@@ -19,11 +21,22 @@ const Header = () => {
             type="text"
             placeholder="Search"
           />
-          <button className="px-2 bg-red-400 rounded ml-2 text-white hover:bg-white hover:text-red-400">Search</button>
+          <button className="px-2 bg-red-400 rounded ml-2 text-white hover:bg-white hover:text-red-400">{props.btnText}</button>
         </section>
       </nav>
     </section>
   );
 };
+
+Header.propTypes = {
+  title: PropTypes.string,
+  aboutText: PropTypes.string,
+  btnText: PropTypes.string
+}
+
+Header.defaultProps = {
+  title: "logo goes here",
+  btnText: "Search"
+}
 
 export default Header;
